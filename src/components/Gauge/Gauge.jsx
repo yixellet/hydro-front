a5a3f5f0001a503dfcbc4d3dba8dbf8d1abc95ee
yixellet <yixwellet@gmail.com>
2021-10-03 21:54:52 +0400

    Создана кнопка "Добавить наблюдения"

--- src/components/Gauge/Gauge.jsx
+++ src/components/Gauge/Gauge.jsx
@@ -69,7 +69,7 @@ class Gauge extends React.Component {
             error ?
             'Ошибка загрузки данных' :
               isFetching ?
-              'Загрузка данных' :
+              'Загрузка данных...' :
               gaugeInfo.name
           }
         </title>
@@ -85,71 +85,81 @@ class Gauge extends React.Component {
             isFetching ?
             <p>ЗАГРУЗКА ДАННЫХ...</p> :
             <>
-            <div className={styles.data}>
+            <div className={styles.nameBlock}>
               <h1 className={styles.title}>{gaugeInfo.name} <span className={styles.river}>({gaugeInfo.stream})</span></h1>
-              <p className={styles.code}>{gaugeInfo.code}</p>
-              <div className={styles.coordsBlock}>
-                <svg width="25" height="25" viewBox="0 0 7.937 7.938" xmlns="http://www.w3.org/2000/svg">
-                  <path className={styles.path} d="M3.969 0C0 0 2.875 5.33 3.969 7.938 5.062 5.33 7.938 0 3.969 0Z"/>
-                  <circle className={styles.circle} cx="3.969" cy="2.106" r="1.196"/>
+              <button className={styles.addButton}>
+                <svg width="18.142" height="18.142" viewBox="0 0 4.8 4.8" xmlns="http://www.w3.org/2000/svg">
+                  <path className={styles.addButtonCross} d="M5.046 2.4H-.246M2.4 5.046V-.246"/>
                 </svg>
-                <p className={styles.coord}>{ddToDms(gaugeInfo.lat)} СШ</p>
-                <p className={styles.coord}>{ddToDms(gaugeInfo.lon)} ВД</p>
-              </div>
-              <div className={styles.refEl_obs}>
-                <ul className={styles.zero}><h2 className={styles.blockTitle}>Абсолютная отметка нуля:</h2>
-                  {
-                    elevs.map((elev, idx) => {
-                      return <li key={idx} className={styles.zero_item}>
-                        <span className={styles.elev}>{elev.elevation.toFixed(2)}</span> (с {dateToStr(elev.startDate, 'word')}{elev.endDate ? ' по ' + dateToStr(elev.endDate, 'word') : null})
-                      </li>
-                    })
-                  }
-                </ul>
-                <div className={styles.block}>
-                  <h2 className={styles.blockTitle}>Наблюдения:</h2>
-                  <Form type='date' />
-                  <Form type='year' action={this.handleGetYearObservations} />
+              </button>
+              <div className={styles.toolTip}>Добавить наблюдения</div>
+            </div>
+            <div className={styles.data_map}>
+              <div className={styles.data}>
+                <p className={styles.code}>{gaugeInfo.code}</p>
+                <div className={styles.coordsBlock}>
+                  <svg width="25" height="25" viewBox="0 0 7.937 7.938" xmlns="http://www.w3.org/2000/svg">
+                    <path className={styles.path} d="M3.969 0C0 0 2.875 5.33 3.969 7.938 5.062 5.33 7.938 0 3.969 0Z"/>
+                    <circle className={styles.circle} cx="3.969" cy="2.106" r="1.196"/>
+                  </svg>
+                  <p className={styles.coord}>{ddToDms(gaugeInfo.lat)} СШ</p>
+                  <p className={styles.coord}>{ddToDms(gaugeInfo.lon)} ВД</p>
+                </div>
+                <div className={styles.refEl_obs}>
+                  <ul className={styles.zero}><h2 className={styles.blockTitle}>Абсолютная отметка нуля:</h2>
+                    {
+                      elevs.map((elev, idx) => {
+                        return <li key={idx} className={styles.zero_item}>
+                          <span className={styles.elev}>{elev.elevation.toFixed(2)}</span> (с {dateToStr(elev.startDate, 'word')}{elev.endDate ? ' по ' + dateToStr(elev.endDate, 'word') : null})
+                        </li>
+                      })
+                    }
+                  </ul>
+                  <div className={styles.block}>
+                    <h2 className={styles.blockTitle}>Наблюдения:</h2>
+                    <Form type='date' />
+                    <Form type='year' action={this.handleGetYearObservations} />
+                  </div>
+                </div>
+                <div className={styles.countValues}>
+                  <ul className={styles.zero}><h2 className={styles.blockTitle}>Расчётные значения:</h2>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-24,83</span> среднемноголетний уровень (безледный период)
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-24,79</span> среднемноголетний уровень (весь период)
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-20,18</span> максимальный уровень (1979 г.)
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-26,87</span> минимальный уровень (1993 г.)
+                    </li>
+                  </ul>
+                  <ul className={styles.zero}><h2 className={styles.blockTitle}>Обеспеченные значения:</h2>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-21,36</span> 1%
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-21,52</span> 3%
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-21,64</span> 5%
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-21,80 </span> 10%
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-22,08 </span> 25%
+                    </li>
+                    <li className={styles.zero_item}>
+                      <span className={styles.elev}>-22,31 </span> 50%
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className={styles.countValues}>
-                <ul className={styles.zero}><h2 className={styles.blockTitle}>Расчётные значения:</h2>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-24,83</span> среднемноголетний уровень (безледный период)
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-24,79</span> среднемноголетний уровень (весь период)
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-20,18</span> максимальный уровень (1979 г.)
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-26,87</span> минимальный уровень (1993 г.)
-                  </li>
-                </ul>
-                <ul className={styles.zero}><h2 className={styles.blockTitle}>Обеспеченные значения:</h2>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-21,36</span> 1%
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-21,52</span> 3%
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-21,64</span> 5%
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-21,80 </span> 10%
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-22,08 </span> 25%
-                  </li>
-                  <li className={styles.zero_item}>
-                    <span className={styles.elev}>-22,31 </span> 50%
-                  </li>
-                </ul>
-              </div>
+              <Map map={mapAstr}/>
             </div>
-            <Map map={mapAstr}/>
             </>
           }
         </article>
